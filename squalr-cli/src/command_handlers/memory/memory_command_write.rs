a0323f48bf89a6591c0c2059475ed69811ad27b1
Zachary Canann <zacharycanann@gmail.com>
2024-06-30 03:42:07 -0700

    Memory writing functional

--- squalr-cli/src/command_handlers/memory/memory_command_write.rs
+++ squalr-cli/src/command_handlers/memory/memory_command_write.rs
@@ -1,16 +1,33 @@
 use crate::command_handlers::memory::memory_command::MemoryCommand;
+use crate::session_manager::SESSION_MANAGER;
 use squalr_engine_common::logging::logger::LOGGER;
 use squalr_engine_common::logging::log_level::LogLevel;
+use squalr_engine_memory::memory_writer::MemoryWriter;
+use squalr_engine_common::dynamic_struct::to_bytes::ToBytes;
 
 pub fn handle_memory_write(cmd: MemoryCommand) {
     if let MemoryCommand::Write { address, value } = cmd {
-        LOGGER.log(
-            LogLevel::Info,
-            &format!(
-                "Writing value {:?} to address {}",
-                value, address
-            ),
-            None
-        );
+        let session_manager = SESSION_MANAGER.lock().unwrap();
+        if let Some(opened_pid) = session_manager.get_opened_process() {
+            // Log the memory write operation
+            LOGGER.log(
+                LogLevel::Info,
+                &format!(
+                    "Writing value {:?} to address {}",
+                    value, address
+                ),
+                None
+            );
+
+            // Perform the memory write operation
+            let memory_writer = MemoryWriter::instance();
+            let memory_writer = memory_writer.lock().expect("Failed to acquire memory writer lock");
+    
+            // Convert value to bytes and write to memory
+            let value_bytes = value.to_bytes();
+            memory_writer.write_bytes(&opened_pid, address, &value_bytes);
+        } else {
+            LOGGER.log(LogLevel::Info, "No process is opened to write to.", None);
+        }
     }
 }
--- squalr-engine-memory/src/memory_writer/mod.rs
+++ squalr-engine-memory/src/memory_writer/mod.rs
@@ -1,5 +1,6 @@
-pub mod memory_writer;
 pub mod memory_writer_trait;
+use std::sync::{Arc, Mutex};
+use crate::memory_writer::memory_writer_trait::IMemoryWriter;
 
 #[cfg(any(target_os = "linux"))]
 mod linux;
@@ -9,3 +10,21 @@ mod macos;
 
 #[cfg(target_os = "windows")]
 mod windows;
+
+#[cfg(target_os = "linux")]
+pub use crate::memory_writer::linux::linux_memory_writer::LinuxMemoryWriter as MemoryWriterImpl;
+
+#[cfg(target_os = "macos")]
+pub use crate::memory_writer::macos::macos_memory_writer::MacOsMemoryWriter as MemoryWriterImpl;
+
+#[cfg(target_os = "windows")]
+pub use crate::memory_writer::windows::windows_memory_writer::WindowsMemoryWriter as MemoryWriterImpl;
+
+
+pub struct MemoryWriter;
+
+impl MemoryWriter {
+    pub fn instance() -> Arc<Mutex<dyn IMemoryWriter>> {
+        Arc::new(Mutex::new(MemoryWriterImpl::new()))
+    }
+}
