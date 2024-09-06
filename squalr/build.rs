73fc5f472fee249271e51cb631f14735abebc761
Zachary Canann <zacharycanann@gmail.com>
2024-09-06 01:59:16 -0700

    Add app icon

--- squalr/build.rs
+++ squalr/build.rs
@@ -1,3 +1,13 @@
 fn main() {
+    // Compile user interface .slint files into usable Rust code.
     slint_build::compile("ui/views/main_window_view.slint").unwrap();
+
+    // Embed windows app icon.
+    if cfg!(target_os = "windows") {
+        let mut res = winresource::WindowsResource::new();
+        res.set_icon("ui/images/app/app_icon.ico")
+            .set("Squalr", "squalr.exe")
+            .set_version_info(winresource::VersionInfo::PRODUCTVERSION, 0x0001000000000000);
+        let _ = res.compile();
+    }
 }
