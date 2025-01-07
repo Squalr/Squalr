dd6f1398f66ff9bc92eddd3dce812e0a40d4f942
Zachary Canann <zacharycanann@gmail.com>
2025-01-06 21:03:29 -0800

    View model collection progress to encapsulate more common logic

--- squalr/src/mvvm/view_model_collection.rs
+++ squalr/src/mvvm/view_model_collection.rs
@@ -1,40 +1,75 @@
-use slint::{ModelRc, VecModel};
+use slint::{ComponentHandle, ModelRc, VecModel, Weak};
 use std::rc::Rc;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
-pub struct ViewModelCollection<T, U>
+pub struct ViewModelCollection<T, U, V>
 where
-    T: Clone + 'static,
+    T: Clone + PartialEq + 'static,
+    U: Send + 'static,
+    V: 'static + ComponentHandle,
 {
     converter: Arc<dyn Fn(U) -> T + Send + Sync>,
+    view_handle: Arc<Mutex<Weak<V>>>,
+    model_setter: Arc<dyn Fn(&V, ModelRc<T>) + Send + Sync>,
 }
 
-impl<T, U> ViewModelCollection<T, U>
+impl<T, U, V> ViewModelCollection<T, U, V>
 where
-    T: Clone + 'static,
+    T: Clone + PartialEq + 'static,
+    U: Send + 'static,
+    V: 'static + ComponentHandle,
 {
-    pub fn new(converter: impl Fn(U) -> T + Send + Sync + 'static) -> Self {
+    pub fn new(
+        view_handle: Weak<V>,
+        converter: impl Fn(U) -> T + Send + Sync + 'static,
+        model_setter: impl Fn(&V, ModelRc<T>) + Send + Sync + 'static,
+    ) -> Self {
         ViewModelCollection {
             converter: Arc::new(converter),
+            view_handle: Arc::new(Mutex::new(view_handle)),
+            model_setter: Arc::new(model_setter),
         }
     }
 
-    pub fn create_empty_model() -> ModelRc<T> {
-        ModelRc::from(Rc::new(VecModel::from(vec![])))
-    }
+    pub fn update_from_source(
+        &self,
+        source_data: Vec<U>,
+    ) {
+        let converter = self.converter.clone();
+        let view_handle = self.view_handle.clone();
+        let model_setter = self.model_setter.clone();
 
-    pub fn converter(&self) -> Arc<dyn Fn(U) -> T + Send + Sync> {
-        self.converter.clone()
+        // Safely get the weak handle
+        let weak_handle = view_handle.lock().unwrap().clone();
+
+        weak_handle
+            .upgrade_in_event_loop(move |handle| {
+                // Do the conversion on the UI thread
+                let converted: Vec<T> = source_data.into_iter().map(|item| (converter)(item)).collect();
+
+                let current_model = Rc::new(VecModel::from(vec![]));
+
+                for item in converted {
+                    current_model.push(item);
+                }
+
+                (model_setter)(&handle, ModelRc::from(current_model));
+            })
+            .expect("Failed to schedule UI update");
     }
 }
 
-impl<T, U> Clone for ViewModelCollection<T, U>
+impl<T, U, V> Clone for ViewModelCollection<T, U, V>
 where
-    T: Clone + 'static,
+    T: Clone + PartialEq + 'static,
+    U: Send + 'static,
+    V: 'static + ComponentHandle,
 {
     fn clone(&self) -> Self {
         ViewModelCollection {
             converter: self.converter.clone(),
+            view_handle: self.view_handle.clone(),
+            model_setter: self.model_setter.clone(),
         }
     }
 }
--- squalr/src/view_models/process_selector/process_selector_view_model.rs
+++ squalr/src/view_models/process_selector/process_selector_view_model.rs
@@ -8,113 +8,90 @@ use crate::mvvm::view_model_collection::ViewModelCollection;
 use crate::mvvm::view_model_entry::ViewModelEntry;
 use crate::view_models::process_selector::process_info_converter::ProcessInfoConverter;
 use slint::ComponentHandle;
-use slint::ModelRc;
-use slint::VecModel;
 use squalr_engine::session_manager::SessionManager;
 use squalr_engine_common::logging::log_level::LogLevel;
 use squalr_engine_common::logging::logger::Logger;
 use squalr_engine_processes::process_info::ProcessInfo;
 use squalr_engine_processes::process_query::process_queryer::ProcessQuery;
 use squalr_engine_processes::process_query::process_queryer::ProcessQueryOptions;
-use std::rc::Rc;
-use std::sync::Arc;
 use sysinfo::Pid;
 
 impl ViewModelEntry for ProcessViewData {}
 
 pub struct ProcessSelectorViewModel {
     view_model_base: ViewModelBase<MainWindowView>,
-    processes: ViewModelCollection<ProcessViewData, ProcessInfo>,
-    windowed_processes: ViewModelCollection<ProcessViewData, ProcessInfo>,
+    processes: ViewModelCollection<ProcessViewData, ProcessInfo, MainWindowView>,
+    windowed_processes: ViewModelCollection<ProcessViewData, ProcessInfo, MainWindowView>,
 }
 
 impl ProcessSelectorViewModel {
     pub fn new(view_model_base: ViewModelBase<MainWindowView>) -> Self {
+        let view_handle = view_model_base.get_view_handle().lock().unwrap().clone();
+
+        let processes = ViewModelCollection::new(
+            view_handle.clone(),
+            |process| ProcessInfoConverter.convert(process),
+            |view: &MainWindowView, model| {
+                view.global::<ProcessSelectorViewModelBindings>()
+                    .set_processes(model)
+            },
+        );
+
+        let windowed_processes = ViewModelCollection::new(
+            view_handle,
+            |process| ProcessInfoConverter.convert(process),
+            |view: &MainWindowView, model| {
+                view.global::<ProcessSelectorViewModelBindings>()
+                    .set_windowed_processes(model)
+            },
+        );
+
         let view = ProcessSelectorViewModel {
-            view_model_base: view_model_base.clone(),
-            processes: ViewModelCollection::new(|process| ProcessInfoConverter.convert(process)),
-            windowed_processes: ViewModelCollection::new(|process| ProcessInfoConverter.convert(process)),
+            view_model_base,
+            processes,
+            windowed_processes,
         };
 
         view.create_view_bindings();
         view
     }
-
-    fn refresh_full_process_list(
-        view_model_base: ViewModelBase<MainWindowView>,
-        converter: Arc<dyn Fn(ProcessInfo) -> ProcessViewData + Send + Sync>,
-    ) {
-        view_model_base.execute_on_ui_thread(move |main_window_view, _| {
-            let process_selector_view = main_window_view.global::<ProcessSelectorViewModelBindings>();
-            let process_query_options = ProcessQueryOptions {
-                required_pid: None,
-                search_name: None,
-                require_windowed: false,
-                match_case: false,
-                fetch_icons: true,
-                limit: None,
-            };
-
-            let processes = ProcessQuery::get_processes(process_query_options);
-            let view_data: Vec<ProcessViewData> = processes
-                .into_iter()
-                .map(|process| converter(process))
-                .collect();
-
-            let new_model = Rc::new(VecModel::default());
-            new_model.set_vec(view_data);
-
-            process_selector_view.set_processes(ModelRc::from(new_model));
-        });
-    }
-
-    fn refresh_windowed_process_list(
-        view_model_base: ViewModelBase<MainWindowView>,
-        converter: Arc<dyn Fn(ProcessInfo) -> ProcessViewData + Send + Sync>,
-    ) {
-        view_model_base.execute_on_ui_thread(move |main_window_view, _| {
-            let process_selector_view = main_window_view.global::<ProcessSelectorViewModelBindings>();
-            let process_query_options = ProcessQueryOptions {
-                required_pid: None,
-                search_name: None,
-                require_windowed: true,
-                match_case: false,
-                fetch_icons: true,
-                limit: None,
-            };
-
-            let processes = ProcessQuery::get_processes(process_query_options);
-            let view_data: Vec<ProcessViewData> = processes
-                .into_iter()
-                .map(|process| converter(process))
-                .collect();
-
-            let new_model = Rc::new(VecModel::default());
-            new_model.set_vec(view_data);
-
-            process_selector_view.set_windowed_processes(ModelRc::from(new_model));
-        });
-    }
 }
 
 impl ViewModel for ProcessSelectorViewModel {
     fn create_view_bindings(&self) {
-        let process_info_converter = self.processes.converter();
+        let process_info_converter = self.processes.clone();
+        let windowed_process_info_converter = self.windowed_processes.clone();
 
         self.view_model_base
-            .execute_on_ui_thread(move |main_window_view, view_model_base| {
+            .execute_on_ui_thread(move |main_window_view, _| {
                 let process_selector_view = main_window_view.global::<ProcessSelectorViewModelBindings>();
 
-                let view_model = view_model_base.clone();
-                let converter = process_info_converter.clone();
                 process_selector_view.on_refresh_full_process_list(move || {
-                    Self::refresh_full_process_list(view_model.clone(), converter.clone());
+                    let process_query_options = ProcessQueryOptions {
+                        required_pid: None,
+                        search_name: None,
+                        require_windowed: false,
+                        match_case: false,
+                        fetch_icons: true,
+                        limit: None,
+                    };
+
+                    let processes = ProcessQuery::get_processes(process_query_options);
+                    process_info_converter.update_from_source(processes);
                 });
 
-                let view_model = view_model_base.clone();
-                let converter = process_info_converter.clone();
                 process_selector_view.on_refresh_windowed_process_list(move || {
-                    Self::refresh_windowed_process_list(view_model.clone(), converter.clone());
+                    let process_query_options = ProcessQueryOptions {
+                        required_pid: None,
+                        search_name: None,
+                        require_windowed: true,
+                        match_case: false,
+                        fetch_icons: true,
+                        limit: None,
+                    };
+
+                    let processes = ProcessQuery::get_processes(process_query_options);
+                    windowed_process_info_converter.update_from_source(processes);
                 });
 
                 process_selector_view.on_select_process(|process_entry| {
