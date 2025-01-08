514aeb5269f0bf2e40e96feafee7da52045076d8
Zachary Canann <zacharycanann@gmail.com>
2025-01-07 17:26:45 -0800

    Support lambdas for view model bindings

--- slint-mvvm-macros/src/lib.rs
+++ slint-mvvm-macros/src/lib.rs
@@ -1,20 +1,3 @@
-// Usage example:
-// create_view_bindings!(
-//     view_binding,
-//     {
-//         SomeBindings => {
-//             on_minimize() -> Self::on_minimize [capture1.clone()],
-//             on_maximize() -> Self::on_maximize [capture1.clone()],
-//             on_close() -> Self::on_close [],
-//             on_drag(delta_x: i32, delta_y: i32) -> Self::on_drag [capture2.clone()]
-//         },
-//         AnotherBindings => {
-//             on_callback(arg: i32) -> Self::other_callback [capture1.clone(), capture2.clone()]
-//         }
-//     }
-// );
-//
-
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
 use syn::{
@@ -22,37 +5,20 @@ use syn::{
     parse::{Parse, ParseStream},
     parse_macro_input,
     punctuated::Punctuated,
-    Expr, Ident, Path, Result as SynResult, Token,
+    Expr, ExprClosure, Ident, Path, Result as SynResult, Token,
 };
 
-/// The main proc-macro entry point for multi-binding creation,
-/// supporting the **NEW** format only:
-///
-/// ```ignore
-/// create_view_bindings!(
-///     some_view_expr,
-///     {
-///         SomeBindings => {
-///             on_minimize() -> Self::on_minimize [view_binding.clone()],
-///             on_maximize() -> Self::on_maximize [view_binding.clone()],
-///             on_close() -> Self::on_close [],
-///             on_drag(delta_x: i32, delta_y: i32) -> Self::on_drag [view_binding.clone()]
-///         },
-///         ...
-///     }
-/// );
-/// ```
+// -------------------------------------------------------------------------------------
+// 1) Top-level macro entry
+// -------------------------------------------------------------------------------------
 #[proc_macro]
 pub fn create_view_bindings(input: TokenStream) -> TokenStream {
     let parsed = parse_macro_input!(input as CreateViewBindingsInput);
     parsed.expand().into()
 }
 
-/// Top-level input for `create_view_bindings!`.
 struct CreateViewBindingsInput {
-    /// The expression (e.g. `view_binding.clone()`) that will call `execute_on_ui_thread`.
     view_expr: Expr,
-    /// One or more binding groups `{ MyBindings => { ... }, OtherBindings => { ... } }`.
     groups: Vec<BindingGroup>,
 }
 
@@ -82,24 +48,12 @@ impl Parse for CreateViewBindingsInput {
 }
 
 impl CreateViewBindingsInput {
-    /// Generates the final macro expansion that calls `execute_on_ui_thread`
-    /// once, and places each binding group inside.
     fn expand(&self) -> proc_macro2::TokenStream {
         let view_expr = &self.view_expr;
         let groups_code = self.groups.iter().map(|group| group.expand());
 
-        // Instead of:
-        //
-        // quote! {
-        //     #view_expr.execute_on_ui_thread(move |main_window_view, view_binding| {
-        //         #(#groups_code)*
-        //     });
-        // }
-        //
-        // do something like this:
         quote! {
             {
-                // Force a clone here
                 let __view_binding = #view_expr.clone();
                 __view_binding.execute_on_ui_thread(move |main_window_view, view_binding| {
                     #(#groups_code)*
@@ -109,7 +63,9 @@ impl CreateViewBindingsInput {
     }
 }
 
-/// Represents each binding group, e.g. `SomeBindings => { on_minimize() -> Self::on_minimize [caps], ... }`.
+// -------------------------------------------------------------------------------------
+// 2) BindingGroup
+// -------------------------------------------------------------------------------------
 struct BindingGroup {
     group_name: Path,
     callbacks: Vec<CallbackDefinition>,
@@ -117,7 +73,7 @@ struct BindingGroup {
 
 impl Parse for BindingGroup {
     fn parse(input: ParseStream) -> SynResult<Self> {
-        let group_name: Path = input.parse()?; // e.g. `WindowViewModelBindings`
+        let group_name: Path = input.parse()?;
         input.parse::<Token![=>]>()?;
 
         let callbacks_block;
@@ -137,8 +93,6 @@ impl Parse for BindingGroup {
 }
 
 impl BindingGroup {
-    /// Expands the group into code that fetches the group bindings
-    /// and registers each callback definition.
     fn expand(&self) -> proc_macro2::TokenStream {
         let group_name = &self.group_name;
         let callback_defs = self.callbacks.iter().map(|cb| cb.expand());
@@ -152,39 +106,53 @@ impl BindingGroup {
     }
 }
 
-/// Represents one callback definition in the new format:
-///
-/// ```ignore
-/// on_something(arg: Type, arg2: Type) -> Self::my_target_fn [cap1.clone(), cap2.clone()]
-/// ```
+// -------------------------------------------------------------------------------------
+// 3) CallbackDefinition (supports a Path or a Closure)
+// -------------------------------------------------------------------------------------
 struct CallbackDefinition {
     callback_name: Ident,
+    // We still parse function-like arguments from `(foo: Type, bar: Type)`,
+    // but we might ignore them if the user writes a closure after `->`.
     args: Vec<(Ident, Ident)>,
-    target_fn: Path,
+    target: CallbackTarget,
     captures: Vec<Expr>,
+}
+
+enum CallbackTarget {
+    Path(Path),
+    Closure(ExprClosure),
 }
 
 impl Parse for CallbackDefinition {
     fn parse(input: ParseStream) -> SynResult<Self> {
-        // Example line:
-        // on_drag(delta_x: i32, delta_y: i32) -> Self::on_drag [view_binding.clone()]
+        // Example lines:
+        //   on_drag(delta_x: i32, delta_y: i32) -> Self::on_drag [view_binding.clone()]
+        //   on_required_write_changed(value: bool) -> |value| { ... } [captures]
 
         // 1) Parse callback_name (e.g. `on_drag`)
         let callback_name: Ident = input.parse()?;
 
-        // 2) Parse parentheses with arguments (e.g. `(delta_x: i32, delta_y: i32)`)
+        // 2) Parse parentheses with arguments if any: (delta_x: i32, delta_y: i32)
         let args_braces;
         parenthesized!(args_braces in input);
         let args = parse_args(&args_braces)?;
 
-        // 3) Parse `->`
+        // 3) Parse '->'
         input.parse::<Token![->]>()?;
 
-        // 4) Parse the path to the target function (e.g. `Self::on_drag`)
-        let target_fn: Path = input.parse()?;
+        // 4) Decide if it's a Path or a Closure
+        let lookahead = input.lookahead1();
+        let target = if lookahead.peek(Token![|]) {
+            // It's a lambda-style closure
+            let expr_closure: ExprClosure = input.parse()?;
+            CallbackTarget::Closure(expr_closure)
+        } else {
+            // It's a path style: e.g. Self::something
+            let path: Path = input.parse()?;
+            CallbackTarget::Path(path)
+        };
 
-        // 5) Parse optional bracketed captures: `[expr, expr, ...]`
-        //    Use `syn::token::Bracket` instead of `Token!['[']`.
+        // 5) Optionally parse bracketed captures: `[expr, expr, ...]`
         let captures = if input.peek(syn::token::Bracket) {
             let bracketed;
             syn::bracketed!(bracketed in input);
@@ -197,40 +165,35 @@ impl Parse for CallbackDefinition {
         Ok(Self {
             callback_name,
             args,
-            target_fn,
+            target,
             captures,
         })
     }
 }
 
-/// Helper to parse a comma-separated list of `(ident: Type)` pairs.
+// Helper to parse a comma-separated list of `(ident: Type)` pairs
 fn parse_args(input: ParseStream) -> SynResult<Vec<(Ident, Ident)>> {
     let mut args = Vec::new();
 
     while !input.is_empty() {
-        // e.g. `delta_x: i32`
         let arg_name: Ident = input.parse()?;
         input.parse::<Token![:]>()?;
         let arg_type: Ident = input.parse()?;
         args.push((arg_name, arg_type));
 
-        // If there's another comma, consume it.
         if input.peek(Token![,]) {
             input.parse::<Token![,]>()?;
         }
     }
-
     Ok(args)
 }
 
+// -------------------------------------------------------------------------------------
+// 4) Expansion
+// -------------------------------------------------------------------------------------
 impl CallbackDefinition {
-    /// Expands into the code that will register the callback with the given captures and arguments.
     fn expand(&self) -> proc_macro2::TokenStream {
         let callback_name = &self.callback_name;
-        let target_fn = &self.target_fn;
-
-        // Create locals for captures, e.g.:
-        // let __cap_0 = view_binding.clone();
         let captures_lets = self.captures.iter().enumerate().map(|(i, cap_expr)| {
             let cap_var = format_ident!("__cap_{}", i);
             quote! {
@@ -238,47 +201,78 @@ impl CallbackDefinition {
             }
         });
 
-        // Prepare the captured variables for calling the function
         let cap_vars = (0..self.captures.len())
             .map(|i| format_ident!("__cap_{}", i))
             .collect::<Vec<_>>();
 
-        // Generate closure arguments: |arg: Type, ...|
-        let arg_patterns = self.args.iter().map(|(arg_name, arg_type)| {
-            quote! { #arg_name: #arg_type }
-        });
+        // We still have (arg_name, arg_type) from the parentheses, though in closure mode,
+        // we might simply ignore them or unify them. For path style, we need them.
+        let arg_patterns = self
+            .args
+            .iter()
+            .map(|(arg_name, arg_type)| quote! { #arg_name: #arg_type });
 
-        // Generate the function call inside the closure
-        let fn_call = if self.captures.is_empty() {
-            // No captures
-            if self.args.is_empty() {
-                // No arguments
-                quote!(#target_fn())
-            } else {
-                // Just arguments
-                let arg_names = self.args.iter().map(|(arg_name, _)| quote!(#arg_name));
-                quote!(#target_fn(#(#arg_names),*))
+        match &self.target {
+            // ---------------------------------------------------------------------------------
+            // Path-based target: e.g. `-> Self::some_fn`
+            // ---------------------------------------------------------------------------------
+            CallbackTarget::Path(path) => {
+                // e.g. Self::on_drag
+                let fn_call = if self.captures.is_empty() {
+                    // No captures
+                    if self.args.is_empty() {
+                        // No arguments
+                        quote!(#path())
+                    } else {
+                        // Just arguments
+                        let arg_names = self.args.iter().map(|(name, _)| quote!(#name));
+                        quote!(#path(#(#arg_names),*))
+                    }
+                } else {
+                    // We have captures
+                    let cloned_caps = cap_vars.iter().map(|v| quote!(#v.clone()));
+                    if self.args.is_empty() {
+                        // Only captures
+                        quote!(#path(#(#cloned_caps),*))
+                    } else {
+                        // Both captures and arguments
+                        let arg_names = self.args.iter().map(|(name, _)| quote!(#name));
+                        quote!(#path(#(#cloned_caps),*, #(#arg_names),*))
+                    }
+                };
+
+                quote! {
+                    {
+                        #(#captures_lets)*
+
+                        group_bindings.#callback_name(move |#(#arg_patterns),*| {
+                            #fn_call
+                        });
+                    }
+                }
             }
-        } else {
-            // We have captures
-            let cloned_caps = cap_vars.iter().map(|var| quote!(#var.clone()));
-            if self.args.is_empty() {
-                // Captures, no arguments
-                quote!(#target_fn(#(#cloned_caps),*))
-            } else {
-                // Both captures and arguments
-                let arg_names = self.args.iter().map(|(arg_name, _)| quote!(#arg_name));
-                quote!(#target_fn(#(#cloned_caps),*, #(#arg_names),*))
-            }
-        };
 
-        quote! {
-            {
-                #(#captures_lets)*
+            // ---------------------------------------------------------------------------------
+            // Closure-based target: e.g. `-> |value| { ... }`
+            // ---------------------------------------------------------------------------------
+            CallbackTarget::Closure(expr_closure) => {
+                // We don’t need to generate a closure in a closure.
+                // We can directly pass the user’s closure to `group_bindings.<callback_name>()`,
+                // but we still wrap it in `move` and handle captures.
+                //
+                // NOTE: If you want to enforce that the user must define the same arguments
+                // in the closure as in `(foo: Type)`, you can add checks or unify them here.
+                // For now, we just ignore the parentheses-based arguments.
+                quote! {
+                    {
+                        #(#captures_lets)*
 
-                group_bindings.#callback_name(move |#(#arg_patterns),*| {
-                    #fn_call
-                });
+                        group_bindings.#callback_name(
+                            // We capture everything, then call the user’s closure
+                            move #expr_closure
+                        );
+                    }
+                }
             }
         }
     }
--- squalr/src/view_models/settings/memory_settings_view_model.rs
+++ squalr/src/view_models/settings/memory_settings_view_model.rs
@@ -15,21 +15,20 @@ impl MemorySettingsViewModel {
             view_binding: view_binding.clone(),
         };
 
-        // Wire up our change handlers via the new multi-binding macro.
         create_view_bindings!(
-            view_binding.clone(),
+            view_binding,
             {
                 MemorySettingsViewModelBindings => {
-                    on_required_write_changed(value: bool) -> Self::set_required_write,
-                    on_required_execute_changed(value: bool) -> Self::set_required_execute,
-                    on_required_copy_on_write_changed(value: bool) -> Self::set_required_copy_on_write,
-                    on_excluded_write_changed(value: bool) -> Self::set_excluded_write,
-                    on_excluded_execute_changed(value: bool) -> Self::set_excluded_execute,
-                    on_excluded_copy_on_write_changed(value: bool) -> Self::set_excluded_copy_on_write,
-                    on_memory_type_none_changed(value: bool) -> Self::set_memory_type_none,
-                    on_memory_type_image_changed(value: bool) -> Self::set_memory_type_image,
-                    on_memory_type_private_changed(value: bool) -> Self::set_memory_type_private,
-                    on_memory_type_mapped_changed(value: bool) -> Self::set_memory_type_mapped
+                    on_required_write_changed(value: bool) -> |value| { MemorySettings::get_instance().set_required_write(value); },
+                    on_required_execute_changed(value: bool) -> |value| { MemorySettings::get_instance().set_required_execute(value); },
+                    on_required_copy_on_write_changed(value: bool) -> |value| { MemorySettings::get_instance().set_required_copy_on_write(value); },
+                    on_excluded_write_changed(value: bool) -> |value| { MemorySettings::get_instance().set_excluded_write(value); },
+                    on_excluded_execute_changed(value: bool) -> |value| { MemorySettings::get_instance().set_excluded_execute(value); },
+                    on_excluded_copy_on_write_changed(value: bool) -> |value| { MemorySettings::get_instance().set_excluded_copy_on_write(value); },
+                    on_memory_type_none_changed(value: bool) -> |value| { MemorySettings::get_instance().set_memory_type_none(value); },
+                    on_memory_type_image_changed(value: bool) -> |value| { MemorySettings::get_instance().set_memory_type_image(value); },
+                    on_memory_type_private_changed(value: bool) -> |value| { MemorySettings::get_instance().set_memory_type_private(value); },
+                    on_memory_type_mapped_changed(value: bool) -> |value| { MemorySettings::get_instance().set_memory_type_mapped(value); },
                 }
             }
         );
@@ -61,45 +60,5 @@ impl MemorySettingsViewModel {
 
             let _implement_me_query_ranges = 5;
         });
-    }
-
-    fn set_required_write(value: bool) {
-        MemorySettings::get_instance().set_required_write(value);
-    }
-
-    fn set_required_execute(value: bool) {
-        MemorySettings::get_instance().set_required_execute(value);
-    }
-
-    fn set_required_copy_on_write(value: bool) {
-        MemorySettings::get_instance().set_required_copy_on_write(value);
-    }
-
-    fn set_excluded_write(value: bool) {
-        MemorySettings::get_instance().set_excluded_write(value);
-    }
-
-    fn set_excluded_execute(value: bool) {
-        MemorySettings::get_instance().set_excluded_execute(value);
-    }
-
-    fn set_excluded_copy_on_write(value: bool) {
-        MemorySettings::get_instance().set_excluded_copy_on_write(value);
-    }
-
-    fn set_memory_type_none(value: bool) {
-        MemorySettings::get_instance().set_memory_type_none(value);
-    }
-
-    fn set_memory_type_image(value: bool) {
-        MemorySettings::get_instance().set_memory_type_image(value);
-    }
-
-    fn set_memory_type_private(value: bool) {
-        MemorySettings::get_instance().set_memory_type_private(value);
-    }
-
-    fn set_memory_type_mapped(value: bool) {
-        MemorySettings::get_instance().set_memory_type_mapped(value);
     }
 }
