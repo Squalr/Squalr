40f6a7d851f9dc76086bb20e35ba76c6c992766f
Zachary Canann <zacharycanann@gmail.com>
2025-03-06 16:21:13 -0800

    Genericize repeated scan logic for big endian float types

--- squalr-engine-common/src/structures/data_types/comparisons/mod.rs
+++ squalr-engine-common/src/structures/data_types/comparisons/mod.rs
@@ -4,5 +4,7 @@ pub mod scalar_comparisons_float_big_endian;
 pub mod scalar_comparisons_integer;
 pub mod scalar_comparisons_integer_big_endian;
 pub mod vector_comparable;
+pub mod vector_comparisons_float;
+pub mod vector_comparisons_float_big_endian;
 pub mod vector_comparisons_integer;
 pub mod vector_comparisons_integer_big_endian;
