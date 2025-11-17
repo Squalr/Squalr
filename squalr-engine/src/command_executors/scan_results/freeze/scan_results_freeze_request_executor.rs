21840d4f26d21ae67608734596c4ba48f03b1d50
Zachary Canann <zacharycanann@gmail.com>
2025-11-16 17:34:16 -0800

    Fix scan result freezing for module based results

--- squalr-engine/src/command_executors/scan_results/freeze/scan_results_freeze_request_executor.rs
+++ squalr-engine/src/command_executors/scan_results/freeze/scan_results_freeze_request_executor.rs
@@ -3,6 +3,8 @@ use crate::engine_privileged_state::EnginePrivilegedState;
 use squalr_engine_api::commands::scan_results::freeze::scan_results_freeze_request::ScanResultsFreezeRequest;
 use squalr_engine_api::commands::scan_results::freeze::scan_results_freeze_response::ScanResultsFreezeResponse;
 use squalr_engine_api::structures::memory::pointer::Pointer;
+use squalr_engine_memory::memory_queryer::memory_queryer::MemoryQueryer;
+use squalr_engine_memory::memory_queryer::memory_queryer_trait::IMemoryQueryer;
 use squalr_engine_memory::memory_reader::MemoryReader;
 use squalr_engine_memory::memory_reader::memory_reader_trait::IMemoryReader;
 use std::sync::Arc;
@@ -42,10 +44,29 @@ impl EngineCommandRequestExecutor for ScanResultsFreezeRequest {
             }
         };
 
+        // Collect modules if possible so that we can resolve whether individual addresses are static later.
+        let modules = if let Some(opened_process_info) = engine_privileged_state
+            .get_process_manager()
+            .get_opened_process()
+        {
+            MemoryQueryer::get_instance().get_modules(&opened_process_info)
+        } else {
+            vec![]
+        };
+
         for scan_result_ref in &self.scan_result_refs {
             if let Some(scan_result) = snapshot_guard.get_scan_result(&symbol_registry, scan_result_ref.get_scan_result_index()) {
                 let address = scan_result.get_address();
-                let pointer = Pointer::new(address, vec![], String::new());
+                let mut module_name = String::default();
+                let mut module_offset = scan_result.get_address();
+
+                // Check whether this scan result belongs to a module (ie check if the address is static).
+                if let Some((found_module_name, address)) = MemoryQueryer::get_instance().address_to_module(module_offset, &modules) {
+                    module_name = found_module_name;
+                    module_offset = address;
+                }
+
+                let pointer = Pointer::new(module_offset, vec![], module_name);
 
                 if self.is_frozen {
                     if let Some(opened_process_info) = engine_privileged_state
